e2607636e1e75bb417e1ed79223d0a49323d2c24
Tsuyoshi Ushio <ushio@simplearchitect.com>
2020-06-28 08:11:31 -0700

    Add Script for uploading

---
+++ script/update_status.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Update Status
+# Upload Status Json to the Blob Strage on Azure. 
+# az long before execture this command
+# -e: immediately exit if any commands has a non-zero exit status
+# -o: prevents errors in a pipeline from being masked
+
+usage() { echo "Usage: update_status.sh -f <fileName> -s <status> -n <name>" 1>&2; exit 1; }
+
+declare storageAccountName="tsushicistate"
+declare container="healthcheck"
+declare fileName=""
+declare status=""
+declare name=""
+
+while getopts ":f:s:n:" arg; do
+    case "${arg}" in
+      f)
+        fileName=${OPTARG}
+      ;;
+      s)
+        status=${OPTARG}
+      ;;
+      n)
+        name=${OPTARG}
+      ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+if [[ -z "$fileName" ]]; then
+  echo "Enter a fileName (e.g. azure-functions-core-tools.json)"
+  usage;
+fi
+
+if [[ -z "$status" ]]; then
+  echo "Enter a status (0: success, 1: warning, 2: error)"  
+  usage;
+fi
+
+if [[ -z "$name" ]]; then
+  echo "Enter a name (e.g. \"Azure Functions Core Tools CI\")"
+  usage;
+fi
+
+currentDate=`date -u +%Y-%m-%dT%H:%M:%S%NZ`
+
+echo "{\"Name\":\"${name}\", \"state\":${status},\"updatedTime\":\"${currentDate}\"}" > ${fileName}
+# If you want to use the --auth-mode login, enable this comment.
+# az role assignment create --role "Storage Blob Data Contributor" --assignee <YOUR_E_MAIL> --scope "/subscriptions/<YOUR_SUBSCRIPTION>/resourceGroups/<YOUR_RESOURCE_GROUP/providers/Microsoft.Storage/storageAccounts/${storageAccountName}/blobServices/default/containers/${container}"
+echo "1-Upload status json to Blob storage (# az storage blob upload --account-name ${storageAccountName} --container-name ${container} --name ${fileName} --<resource-group>file ${fileName} )"
+az storage blob upload --account-name ${storageAccountName} --container-name ${container} --name ${fileName} --file ${fileName} # --auth-mode login
